bc93a641b43adaa93c1af2216b8ce2ab64fdcf84
Sanlawshu <z0918583782@gmail.com>
2023-06-07 12:28:06 +0800

    6/7

--- lib/widgets/api.dart
+++ lib/widgets/api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class API {
 
-  String ngrokUrl = "d961-61-61-81-242.jp.ngrok.io";
+  String ngrokUrl = "aa0b-61-61-81-242.jp.ngrok.io";
 
   Future<http.Response> createUser(
       String name, int age, String gender, String email, String password) {
